0e8f946d2ec4aeff36165a8c77f55a74e5c48105
Blaise Baptist <blaise.baptist@gmail.com>
2026-02-25 17:45:46 -0500

    stuff idk to early for good commits

--- src/board.rs
+++ src/board.rs
@@ -17,13 +17,19 @@ pub struct State {
 
 impl std::default::Default for State {
     fn default() -> Self {
-        State {
+        let mut new_state = State {
             grid: [Tile::default(); ROWS * COLS],
             time: 0,
             active_piece: Piece::default(),
             bag: [None; 7],
             playing: true,
-        }
+        };
+        new_state.tick();
+        new_state.tick();
+        new_state.tick();
+        new_state.tick();
+        new_state.tick();
+        new_state
     }
 }
 impl State {
@@ -37,7 +43,8 @@ impl State {
             return;
         }
         self.active_piece = piece;
-        self.playing = self.check_fall();
+        self.playing = self.check_move(COLS.try_into().unwrap(), true) == MoveResult::Moved;
+        println!("adding new piece playing = {}", self.playing);
         self.active_piece.places.iter_mut().for_each(|index| {
             *index += 4;
             self.grid[*index].has_tile = true
@@ -45,25 +52,82 @@ impl State {
     }
     pub fn tick(&mut self) {
         self.time += 1;
-        if self.check_fall() {
-            self.active_piece.places.iter_mut().for_each(|index| {
-                self.grid[*index].has_tile = false;
-                *index += 10;
-                self.grid[*index].has_tile = true;
-            })
-        } else {
-            self.add_piece(Piece::default());
+        if self.time % 5 == 0 {
+            if self.move_pieces(10, true) == MoveResult::HitBottom {
+                self.add_piece(Piece::default());
+            }
         }
     }
-    fn check_fall(&self) -> bool {
-        self.active_piece.places.iter().all(|index| {
-            if let Some(tile) = self.grid.get(index + 10) {
-                !tile.has_tile
-            } else {
-                false
-            }
-        })
+    pub fn take_action(&mut self, action: super::Action) {
+        // println!("{:?}", self);
+        match action {
+            super::Action::Left => self.move_pieces(-1, false),
+            super::Action::Right => self.move_pieces(1, false),
+            super::Action::FinishFall => MoveResult::HitBottom,
+            super::Action::CounterClockWise => self.spin_pieces(false),
+            super::Action::ClockWise => self.spin_pieces(true),
+        };
     }
+    fn check_move(&self, amount: isize, can_wrap: bool) -> MoveResult {
+        for index in self.active_piece.places.iter() {
+            let new_index = amount.checked_add_unsigned(*index).unwrap();
+            if new_index.is_negative() {
+                // println!("moving before first square");
+                return MoveResult::OutOfBounds;
+            }
+            let new_index = new_index.unsigned_abs();
+            if new_index >= (ROWS * COLS) {
+                // println!("moving off edge of board");
+                return MoveResult::HitBottom;
+            }
+            if amount < 0 && !can_wrap {
+                if (new_index + 1).is_multiple_of(COLS) {
+                    // println!("trying to wrap when can_wrap = {}", can_wrap);
+                    return MoveResult::NoWrapAllowed;
+                }
+            }
+            if amount > 0 && !can_wrap {
+                if new_index.is_multiple_of(COLS) {
+                    // println!("trying to wrap when can_wrap = {}", can_wrap);
+                    return MoveResult::NoWrapAllowed;
+                }
+            }
+            if !self.active_piece.places.contains(&new_index) && self.grid[new_index].has_tile {
+                return MoveResult::HitBottom;
+            }
+        }
+        return MoveResult::Moved;
+    }
+    fn move_pieces(&mut self, amount: isize, can_wrap: bool) -> MoveResult {
+        let check_move_result = self.check_move(amount, can_wrap);
+        if check_move_result != MoveResult::Moved {
+            return check_move_result;
+        }
+        self.active_piece
+            .places
+            .iter()
+            .for_each(|index| self.grid[*index].has_tile = false);
+        self.active_piece
+            .places
+            .iter_mut()
+            .for_each(|index| *index = (*index).checked_add_signed(amount).unwrap());
+        self.active_piece
+            .places
+            .iter()
+            .for_each(|index| self.grid[*index].has_tile = true);
+        return check_move_result;
+    }
+    fn spin_pieces(&mut self, clockwise: bool) -> MoveResult {
+        //TODO
+        todo!();
+    }
+}
+#[derive(PartialEq, Debug)]
+enum MoveResult {
+    NoWrapAllowed,
+    HitBottom,
+    Moved,
+    OutOfBounds,
 }
 #[derive(Debug, Copy, Clone)]
 struct Piece {
@@ -76,7 +140,6 @@ impl std::default::Default for Piece {
         }
     }
 }
-impl Piece {}
 #[derive(Debug, Copy, Clone)]
 enum Tetrominoe {
     J,
--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,56 @@
 mod board;
+use iced::keyboard;
 use iced::widget::container;
 use iced::{time, Element, Fill, Subscription};
 use std::time::Duration;
 #[derive(Debug, Clone)]
 enum Message {
     Tick,
+    TakeAction(Action),
+}
+#[derive(Debug, Clone)]
+enum Action {
+    Left,
+    Right,
+    ClockWise,
+    CounterClockWise,
+    FinishFall,
 }
 pub fn main() -> iced::Result {
     iced::application(board::State::default, update, view)
-        .subscription(timer)
+        .subscription(subscription)
         .run()
 }
-fn timer(_state: &board::State) -> Subscription<Message> {
-    time::every(Duration::from_secs_f64(0.05)).map(|_| Message::Tick)
+fn subscription(_state: &board::State) -> Subscription<Message> {
+    Subscription::batch([timer(), player_input()])
 }
+fn timer() -> Subscription<Message> {
+    time::every(Duration::from_secs_f64(0.01)).map(|_| Message::Tick)
+}
+fn player_input() -> Subscription<Message> {
+    keyboard::listen().filter_map(|event| {
+        if let keyboard::Event::KeyPressed {
+            key: keyboard::Key::Character(key),
+            ..
+        } = event
+        {
+            return match key.as_ref() {
+                "a" => Some(Message::TakeAction(Action::Left)),
+                "d" => Some(Message::TakeAction(Action::Right)),
+                _ => None,
+            };
+        }
+        None
+    })
+}
+
 fn update(state: &mut board::State, message: Message) {
     match message {
         Message::Tick => {
             state.tick();
+        }
+        Message::TakeAction(action) => {
+            state.take_action(action);
         }
     }
 }
