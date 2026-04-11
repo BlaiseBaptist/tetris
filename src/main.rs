27cdc3b2b26916ee7320adf0979dd8dc54b5f9d3
Andrew Baptist <andrew.baptist@gmail.com>
2026-04-11 19:28:33 -0400

    first commit

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,13 @@ use iced::keyboard;
 use iced::widget::container;
 use iced::{time, Element, Fill, Subscription};
 use std::time::Duration;
+
 #[derive(Debug, Clone)]
 enum Message {
     Tick,
     TakeAction(Action),
 }
+
 #[derive(Debug, Clone)]
 enum Action {
     Left,
@@ -15,32 +17,58 @@ enum Action {
     ClockWise,
     CounterClockWise,
     FinishFall,
+    SoftDrop,
 }
+
 pub fn main() -> iced::Result {
     iced::application(board::State::default, update, view)
         .subscription(subscription)
         .run()
 }
+
 fn subscription(_state: &board::State) -> Subscription<Message> {
     Subscription::batch([timer(), player_input()])
 }
+
 fn timer() -> Subscription<Message> {
     time::every(Duration::from_secs_f64(0.01)).map(|_| Message::Tick)
 }
+
 fn player_input() -> Subscription<Message> {
     keyboard::listen().filter_map(|event| {
-        if let keyboard::Event::KeyPressed {
-            key: keyboard::Key::Character(key),
-            ..
-        } = event
-        {
-            return match key.as_ref() {
-                "a" => Some(Message::TakeAction(Action::Left)),
-                "d" => Some(Message::TakeAction(Action::Right)),
+        if let keyboard::Event::KeyPressed { key, .. } = event {
+            match key {
+                keyboard::Key::Character(ref c) => match c.as_ref() {
+                    "a" => Some(Message::TakeAction(Action::Left)),
+                    "d" => Some(Message::TakeAction(Action::Right)),
+                    "w" => Some(Message::TakeAction(Action::ClockWise)),
+                    "q" => Some(Message::TakeAction(Action::CounterClockWise)),
+                    "s" => Some(Message::TakeAction(Action::SoftDrop)),
+                    _ => None,
+                },
+                keyboard::Key::Named(named) => match named {
+                    keyboard::key::Named::ArrowLeft => {
+                        Some(Message::TakeAction(Action::Left))
+                    }
+                    keyboard::key::Named::ArrowRight => {
+                        Some(Message::TakeAction(Action::Right))
+                    }
+                    keyboard::key::Named::ArrowUp => {
+                        Some(Message::TakeAction(Action::ClockWise))
+                    }
+                    keyboard::key::Named::ArrowDown => {
+                        Some(Message::TakeAction(Action::SoftDrop))
+                    }
+                    keyboard::key::Named::Space => {
+                        Some(Message::TakeAction(Action::FinishFall))
+                    }
+                    _ => None,
+                },
                 _ => None,
-            };
+            }
+        } else {
+            None
         }
-        None
     })
 }
 
